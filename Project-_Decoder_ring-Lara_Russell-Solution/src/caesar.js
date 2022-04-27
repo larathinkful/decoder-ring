7fd4efef9164e36052110f4f6dff19250bf1a5b6
Lara <99837142+larathinkful@users.noreply.github.com>
2022-04-26 21:16:36 -0500

    add notes and begin code

--- Project-_Decoder_ring-Lara_Russell-Solution/src/caesar.js
+++ Project-_Decoder_ring-Lara_Russell-Solution/src/caesar.js
@@ -1,13 +1,23 @@
 // Please refrain from tampering with the setup code provided here,
 // as the index.html and test files rely on this setup to work properly.
 // Only add code (helper methods, variables, etc.) within the scope
-// of the anonymous function on line 6
+// of the anonymous function on line 12
+
+/*Instructional notes: If the shift value isn't present, equal to 0, less than -25, or greater than 25, the function should return false.
+A positive number means shifting to the right whereas a negative number means shifting to the left (i.e., M becomes K).
+Spaces should be maintained throughout, as should other nonalphabetic symbols.
+Capital letters can be ignored.
+If a letter is shifted so that it goes "off" the alphabet (e.g., a shift of 3 on the letter z), it should wrap around to the front of the alphabet (e.g., z becomes c). */
 
 const caesarModule = (function () {
   // you can add any code you want within this function scope
 
+ // note to self: will want to use charAt()
   function caesar(input, shift, encode = true) {
-    // your solution code here
+    for (let i = 0; i<input.length, i++){
+      const temp = input.charAt(i);
+      
+    
   }
 
   return {
